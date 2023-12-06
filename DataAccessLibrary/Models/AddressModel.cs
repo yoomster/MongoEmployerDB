4f45aadb90e43f352c90d7f05abe35ef5cfb6424
yoomster <nperenboom@hotmail.com>
2023-12-06 13:42:23 +0100

    added models folder + address model

---
+++ DataAccessLibrary/Models/AddressModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public class AddressModel
+    {
+        public string AddressName { get; set; }
+        public string HouseNumber { get; set; }
+        public string Zipcode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
--- DataAccessLibrary/Models/PersonModel.cs
+++ DataAccessLibrary/Models/PersonModel.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLibrary
+namespace DataAccessLibrary.Models
 {
     public class PersonModel
     {
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public AddressModel Addresses{ get; set; }
+        public AddressModel Addresses { get; set; }
     }
 }
