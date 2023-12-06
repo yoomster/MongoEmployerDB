98b77f832fc584601ee73a03ebfadb1cbcc7ec54
yoomster <nperenboom@hotmail.com>
2023-12-06 13:14:28 +0100

    Added method to get the connection string + added nugget package

--- MongoEmployerDB/Program.cs
+++ MongoEmployerDB/Program.cs
@@ -1,13 +1,29 @@
-﻿namespace MongoEmployerDB
+﻿using Microsoft.Extensions.Configuration;
+using MongoDBDataAccess;
+
+
+namespace MongoEmployerDB
 {
     class Program
     {
+        private static MongoDBDataAccess db;
         static void Main(string[] args)
-        {
-            
-        }
-        private void GetConnectionString()
         {
 
         }
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
+        }
+    }
 }
