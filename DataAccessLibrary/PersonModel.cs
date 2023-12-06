cc4842fa1d090063b4cf878e35b471b3eb190dea
yoomster <nperenboom@hotmail.com>
2023-12-06 13:12:48 +0100

    Added library for data access

---
+++ DataAccessLibrary/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class PersonModel
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AddressModel Addresses{ get; set; }
+    }
+}
