553a3c618db4f43a95c57dc985fa01970f636b79
yoomster <nperenboom@hotmail.com>
2023-12-06 16:11:53 +0100

    Added the constructor for creating connection to db

---
+++ DataAccessLibrary/MongoDBDataAccess.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class MongoDBDataAccess
+    {
+        private IMongoDatabase db;
+        public MongoDBDataAccess(string dbName, string connectionString)
+        {
+            //connect to local machine
+            var client = new MongoClient(connectionString);
+
+            //connect to specified db, on specified MongoDB server
+            db = client.GetDatabase(dbName);
+        }
+    }
+}
